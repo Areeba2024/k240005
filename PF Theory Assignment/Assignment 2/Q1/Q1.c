fdcc036980bffb7837fc32ec99b1d727cf8cf004
Areeba2024 <areeba.062005@gmail.com>
2024-10-19 20:19:15 +0500

    Add files via upload

---
+++ PF Theory Assignment/Assignment 2/Q1/Q1.c
@@ -0,0 +1,41 @@
+//NAME:AREEBA IMRAN
+//ID : 24K-0005
+//QUESTION 1
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+	int len,temp,i;
+	printf("Enter the length of the array: ");
+	scanf("%d",&len);
+	int arr[len];
+	printf("Input %d elements in the array(value must be <9999) \n",len);
+	for(i=0;i<len;i++)
+	{
+		printf("element-%d: ",i);
+		scanf("%d",&arr[i]);
+	}
+	bool flag = true;
+	while(flag==true)
+	{
+		flag = false;
+		for(i=0;i<len-1;i++)
+		{
+			if(arr[i]>arr[i+1])
+			{
+				temp = arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]= temp;
+				flag = true;
+			}
+		}
+	}
+	if(len >= 2)
+	{
+	    printf("The Second smallest element in the array is: %d",arr[1]);
+	}
+	else
+	{
+	    printf("Doesn't have enough elements");
+	}
+}
